60c4ce9a1dc65a554e2ddb27c3257aaccf910e3b
Felipe Rubio <rubio@Felipes-Mac-mini.local>
2024-10-23 06:09:34 -0500

    Product detail search func add to product service

--- ChallengeMeLi/ChallengeMeLi/Services/MockUpProductService.swift
+++ ChallengeMeLi/ChallengeMeLi/Services/MockUpProductService.swift
@@ -7,6 +7,12 @@
 import Foundation
 
 public struct MockUpProductService: ProductServiceProtocol {
+    public func fetchProductDetail(for productId: String, completion: @escaping (Result<ProductDetail, ProductServiceError>) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
+            completion(.success(ProductDetail.random()))
+        }
+    }
+    
     public func fetchProducts(with query: String, completion: @escaping (Result<[Product], ProductServiceError>) -> Void) {
         DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
             let mockProducts = (1...20).map { _ in Product.random() }
