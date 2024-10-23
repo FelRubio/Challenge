31a50ac1ab19349f57c7d5930fc0b9a8ff3a2531
Felipe Rubio <rubio@Felipes-Mac-mini.local>
2024-10-23 01:43:15 -0500

    Project creation

---
+++ ChallengeMeLi/ChallengeMeLi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeMeLi
+//
+//  Created by Felipe Rubio on 23/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .gray
+        self.navigationItem.title = "Challenge MeLi"
+    }
+}
+
